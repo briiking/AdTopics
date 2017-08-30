c216eebfd5a80f69c2b26b21477ba2a4f83ffa2d
briiking <31250046+briiking@users.noreply.github.com>
2017-08-30 07:58:57 -0700

    Add files via upload

---
+++ silly_string.rb
@@ -0,0 +1,9 @@
+#code your solutions here
+puts "lit".upcase #1 #2
+puts "IM NOT SHOUTING".downcase
+puts "supercalifragilisticexpialidocious".size #4
+puts "wow" + " mom" #5
+puts "i".upcase + " really like programming"
+a= "Astounding aardvarks, arguably an ancient animal, always ate apples and acorns and artichoke"
+puts a.gsub! 'A','O'
+puts a.gsub! 'a','o'
